947f37ea09cc37b977c79053ac760de953ce27f6
HysteriasKiwo <cerynna@gmail.com>
2017-09-27 13:18:22 +0200

    Fix BUG

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require "TimeTravel.php";
 
-$marty = new \wcs\TimeTravel(new DateTime('1985-12-31 08:10:20'), new DateTime('1984-02-20 09:20:10'));
+$marty = new \wcs\TimeTravel(1985-12-31 08:10:20', '1984-02-20 09:20:10');
 
 $marty->getTravelInfo();
 
